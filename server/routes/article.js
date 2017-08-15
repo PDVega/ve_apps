61bd2907cd9782be62bcbcaa0c9d1f84646719a3
pdvega <pdvegaa@gmail.com>
2017-08-15 14:25:43 +0700

    install webpack frontend

--- server/routes/article.js
+++ server/routes/article.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../controllers/article_controller')
 
+//article API
 router.post('/', Controller.post)
 router.get('/', Controller.getAll)
 router.get('/:id', Controller.getOne)
 router.put('/:id', Controller.update)
 router.delete('/:id', Controller.destroy)
+
+//get by author
+
+//get by category
 
 module.exports = router;
