5fe7e46c75ac06b5142dbd12d734d8cf4c9bfcec
pdvega <pdvegaa@gmail.com>
2017-08-15 16:07:47 +0700

    navbar menu dan article udah muncul

---
+++ client/livecodephasetwo/src/components/ArticleSummary.vue
@@ -0,0 +1,25 @@
+<template>
+  <router-link :to="'/blog/' + article.id" class="list-group-item">
+    <div class="row">
+      <div class="col-md-4">
+        <img :src="article.image" class="img-responsive"/>
+      </div>
+      <div class="col-md-8">
+        <router-link :to="'/blog/' + article.id">
+          <h4 class="list-group-item-heading">
+            {{ article.title }}
+          </h4>
+        </router-link>
+        <p class="list-group-item-text">
+          {{ article.content.substring(0, 20) }} . . .
+        </p>
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: ['article']
+}
+</script>
--- client/livecodephasetwo/src/router/index.js
+++ client/livecodephasetwo/src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@components/Home'
-import MainContent from '@components/MainContent'
-import ArticleDetail from '@components/ArticleDetail'
+import Home from '@/components/Home'
+import MainContent from '@/components/MainContent'
+import ArticleDetail from '@/components/ArticleDetail'
 
 Vue.use(Router)
 
